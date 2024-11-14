05a4f86b5fe5bb3ce7fc249fb20eaba03b4fd04d
Ahri <damion.jakob@gmail.com>
2024-11-13 23:24:51 -0500

    Changed Nav to a JS Script

--- public/scripts/navigate.js
+++ public/scripts/navigate.js
@@ -1,10 +1,19 @@
 // Written By: Damion Murcell
 // Purpose: This script is for any navigations in the pages
 
-// Navigation For Nav Bar (In The Future)
+// ====== Navigation For Nav Bar ====== 
+document.getElementById("btnNavHome").addEventListener('click', function(){
+    window.location.href = "index.html"
+})
+document.getElementById("btnNavCreateNCR").addEventListener('click', function(){
+    window.location.href = "create.html"
+})
 
+document.getElementById("btnNavNCRs").addEventListener('click', function(){
+    window.location.href = "list.html"
+})
 
-// Navigation For Home Page Buttons
+// ====== Navigation For Home Page Buttons ====== 
 document.getElementById("btnCreateNCR").addEventListener('click', function(){
     window.location.href = 'create.html'
 })
