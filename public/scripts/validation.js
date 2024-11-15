bf10d19602cf6c96a3528b98d8fed4dbf5a418e7
RawicePhil <rawicephil@gmail.com>
2024-11-15 01:57:04 -0500

    Added Validation(Not Working) / Missing Fields

--- public/scripts/validation.js
+++ public/scripts/validation.js
@@ -1,20 +1,51 @@
 /// Field Validations
-document.getElementById("qualityView").addEventListener("submit", function(event){
 
+//Random Number Generator
+//function getRandomInt(max) {
+//    return Math.floor(Math.random() * max);
+//}
 
+//Required variables
+const currentDate = new Date();
+//var randomNumber = getRandomInt(999999999)
 
-    //Quantity Received
-    const quantityRec = document.getElementById("QuantityReceived").value
+//On page load events
+//document.getElementById("QADate").value = currentDate
+//document.getElementById("SalesNumber").text.value = randomNumber
 
-    if (quantityRec <= 0) {
-        alert("")
-    }
-    //Quantity Defective
+//Submit button event handler
+ncrform.addEventListener("submit", (e) =>{
 
+    //GetElements
+    const quantityRec = parseInt(document.getElementById("QuantityDefective").value);
+    const quantityDef = parseInt(document.getElementById("QuantityDefective").value);
+    const qaDate = document.getElementById("QADate").value;
 
-    //Date
-
-
+    //Functions
+    QuantityRecValidation();
+    QuantityDefValidation();
+    QADateValidation();
 });
 
+    //Quantity Received
+function QuantityRecValidation(quantityRec){
+    if (quantityRec > 0) {
+        alert("Quantity Received has to be greater than 0");
+    }
+};
+
+    //Quantity Defective
+function QuantityDefValidation(quantityDef, quantityRec){
+
+    if (quantityDef < quantityRec) {
+        alert("Quantity Defective cannot be greater than Quantity Recieved");
+    }
+};
+    
+    //Quality Date
+function QADateValidation(qaDate, currentDate){
+    if (qaDate < currentDate) {
+        alert("Date cannot be in the past");
+    }
+};
 
