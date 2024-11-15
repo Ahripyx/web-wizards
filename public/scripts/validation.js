dc33b6ca96bf940c8e847f0124a3b9fe7990bf70
RawicePhil <rawicephil@gmail.com>
2024-11-15 00:27:34 -0500

    Some files completed, had to restart computer  :(

---
+++ public/scripts/validation.js
@@ -0,0 +1,20 @@
+/// Field Validations
+document.getElementById("qualityView").addEventListener("submit", function(event){
+
+
+
+    //Quantity Received
+    const quantityRec = document.getElementById("QuantityReceived").value
+
+    if (quantityRec <= 0) {
+        alert("")
+    }
+    //Quantity Defective
+
+
+    //Date
+
+
+});
+
+
